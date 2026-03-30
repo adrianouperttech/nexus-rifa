0e47530b14d120bc09cde475318a0b56088c8328
adriano.uperttech <adriano.uperttech@gmail.com>
2026-03-30 19:39:06 +0000

    Refatorando as estruturas separando, api-server, mobile-app, web-app

---
+++ apps/api-server/src/modules/rifas/rifas.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { RifasController } from './rifas.controller';
+import { RifasService } from './rifas.service';
+import { Rifa } from './entities/rifa.entity';
+import { CotasModule } from '../cotas/cotas.module';
+import { BillingModule } from '../billing/billing.module';
+import { PlansModule } from '../plans/plans.module';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Rifa]), CotasModule, BillingModule, PlansModule],
+  controllers: [RifasController],
+  providers: [RifasService],
+  exports: [RifasService],
+})
+export class RifasModule {}
