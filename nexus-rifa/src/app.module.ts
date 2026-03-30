8e3d3d0e1544beaf4ad5e81eb57e7f0e50d7e3f0
adriano.uperttech <adriano.uperttech@gmail.com>
2026-03-30 17:03:30 +0000

    Versão 1.0 - Api - ajuste app.module.ts

--- nexus-rifa/src/app.module.ts
+++ nexus-rifa/src/app.module.ts
@@ -1,16 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AuthModule } from '../auth/auth.module';
-import { UserModule } from '../user/user.module';
-import { RifasModule } from '../rifas/rifas.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { RifasModule } from './rifas/rifas.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    // Esta é agora a ÚNICA configuração do TypeORM em toda a sua aplicação.
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
@@ -23,12 +22,11 @@ import { RifasModule } from '../rifas/rifas.module';
         return {
           type: 'postgres',
           url: databaseUrl,
-          autoLoadEntities: true, // Encontra automaticamente todas as suas entidades
-          synchronize: false, // NUNCA usar synchronize: true em produção
+          autoLoadEntities: true,
+          synchronize: false,
           ssl: {
-            rejectUnauthorized: false, // Necessário para a maioria das conexões de BD em nuvem
+            rejectUnauthorized: false,
           },
-          // A CORREÇÃO DEFINITIVA: Força o driver do banco de dados a usar a rede IPv4
           extra: {
             family: 4,
           },
--- nexus-rifa/src/seeder.ts
+++ nexus-rifa/src/seeder.ts
@@ -1,16 +1,16 @@
 import { seeder } from 'nestjs-seeder';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
-import { RootUser } from './modules/root-users/entities/root-user.entity';
-import { RootUserSeeder } from './database/seeders/root-user.seeder';
+import { User } from './user/user.entity'; // Assumindo que o nome da sua entidade de usuário é User e está neste caminho
+import { UserSeeder } from './database/seeders/user.seeder'; // Assumindo que o seeder de usuário é este
+import { AppModule } from './app.module'; // Importa o AppModule que tem a configuração do BD
+
+// Por favor, note que tive que fazer algumas suposições sobre os nomes e localizações 
+// das suas entidades e seeders de usuário. Se o build falhar novamente, por favor,
+// verifique se os caminhos './user/user.entity' e './database/seeders/user.seeder' estão corretos.
 
 seeder({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    DatabaseModule,
-    TypeOrmModule.forFeature([RootUser]),
+    AppModule, // O AppModule já importa o ConfigModule e o TypeOrmModule.forRootAsync
+    TypeOrmModule.forFeature([User]), // Você ainda precisa especificar qual entidade este seeder usará
   ],
-}).run([RootUserSeeder]);
+}).run([UserSeeder]);
