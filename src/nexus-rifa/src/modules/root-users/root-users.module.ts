e70bb0b25366e83bb32c2e073f84d8306a524727
adrianouperttech <adriano.uperttech@gmail.com>
2026-03-30 15:40:42 -0400

    Delete src/nexus-rifa/src/modules directory

--- src/nexus-rifa/src/modules/root-users/root-users.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { RootUsersController } from './root-users.controller';
-import { RootUsersService } from './root-users.service';
-
-@Module({
-  controllers: [RootUsersController],
-  providers: [RootUsersService]
-})
-export class RootUsersModule {}
