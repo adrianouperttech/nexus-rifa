86187b62eee24fb0306e30fcd943c29a1deba413
adriano.uperttech <adriano.uperttech@gmail.com>
2026-03-30 19:27:07 +0000

    Versão 1.1 - Api - ajuste render

--- nexus-rifa/src/modules/reservas/reservas.module.ts
+++ nexus-rifa/src/modules/reservas/reservas.module.ts
@@ -4,11 +4,16 @@ import { ReservasController } from './reservas.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Reserva } from './entities/reserva.entity';
 import { TenantsModule } from '../tenants/tenants.module';
+import { RifasModule } from '../rifas/rifas.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Reserva]), forwardRef(() => TenantsModule)],
+  imports: [
+    TypeOrmModule.forFeature([Reserva]),
+    forwardRef(() => TenantsModule),
+    forwardRef(() => RifasModule),
+  ],
   controllers: [ReservasController],
   providers: [ReservasService],
-  exports: [ReservasService] // Adicionado para que outros módulos possam usar
+  exports: [ReservasService],
 })
 export class ReservasModule {}
