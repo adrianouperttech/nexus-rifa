70c7968f96763a3ea2328881045dee371703b311
adriano.uperttech <adriano.uperttech@gmail.com>
2026-03-30 19:20:44 +0000

    Versão 1.1 - Api - ajuste render

--- nexus-rifa/src/modules/reservas/reservas.module.ts
+++ nexus-rifa/src/modules/reservas/reservas.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { ReservasService } from './reservas.service';
 import { ReservasController } from './reservas.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,8 +6,9 @@ import { Reserva } from './entities/reserva.entity';
 import { TenantsModule } from '../tenants/tenants.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Reserva]), TenantsModule],
+  imports: [TypeOrmModule.forFeature([Reserva]), forwardRef(() => TenantsModule)],
   controllers: [ReservasController],
   providers: [ReservasService],
+  exports: [ReservasService] // Adicionado para que outros módulos possam usar
 })
 export class ReservasModule {}
--- nexus-rifa/src/modules/tenants/tenants.module.ts
+++ nexus-rifa/src/modules/tenants/tenants.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TenantsService } from './tenants.service';
 import { TenantsController } from './tenants.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tenant } from './entities/tenant.entity';
 import { UsersModule } from '../users/users.module';
 import { MulterModule } from '@nestjs/platform-express';
+import { ReservasModule } from '../reservas/reservas.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tenant]), UsersModule, MulterModule],
+  imports: [TypeOrmModule.forFeature([Tenant]), UsersModule, MulterModule, forwardRef(() => ReservasModule)],
   controllers: [TenantsController],
   providers: [TenantsService],
   exports: [TenantsService],
