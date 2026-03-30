a9752caed8b1276580c1f2476a6cdb7c46dc419f
adriano.uperttech <adriano.uperttech@gmail.com>
2026-03-30 18:38:27 +0000

    Versão 1.1 - Api - ajuste render orm

--- nexus-rifa/src/app.module.ts
+++ nexus-rifa/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
 import { RifasModule } from './modules/rifas/rifas.module';
 import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
-import ormconfig from '../ormconfig';
 
 @Module({
   imports: [
@@ -17,7 +16,26 @@ import ormconfig from '../ormconfig';
       ttl: 60000, // 60 segundos
       limit: 60, // 60 requisições
     }]),
-    TypeOrmModule.forRoot(ormconfig),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const databaseUrl = configService.get<string>('DATABASE_URL');
+        if (!databaseUrl) {
+          throw new Error('A variável de ambiente DATABASE_URL não está definida.');
+        }
+
+        return {
+          type: 'postgres',
+          url: databaseUrl,
+          entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+          synchronize: true, // Alterado para true para desenvolvimento
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        };
+      },
+    }),
     AuthModule,
     UsersModule,
     RifasModule,
@@ -25,7 +43,7 @@ import ormconfig from '../ormconfig';
   providers: [
     {
       provide: APP_GUARD,
-      useClass: ThrottlerGuard, 
+      useClass: ThrottlerGuard,
     },
   ],
 })
