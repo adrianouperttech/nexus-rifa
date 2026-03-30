b656a39b0b5de7bd8c919ec017bd2a4b04a82601
adriano.uperttech <adriano.uperttech@gmail.com>
2026-03-30 19:08:26 +0000

    Versão 1.1 - Api - ajuste render orm

--- nexus-rifa/src/modules/rifas/rifas.module.ts
+++ nexus-rifa/src/modules/rifas/rifas.module.ts
@@ -4,9 +4,11 @@ import { RifasController } from './rifas.controller';
 import { RifasService } from './rifas.service';
 import { Rifa } from './entities/rifa.entity';
 import { CotasModule } from '../cotas/cotas.module';
+import { BillingModule } from '../billing/billing.module';
+import { PlansModule } from '../plans/plans.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Rifa]), CotasModule],
+  imports: [TypeOrmModule.forFeature([Rifa]), CotasModule, BillingModule, PlansModule],
   controllers: [RifasController],
   providers: [RifasService],
 })
