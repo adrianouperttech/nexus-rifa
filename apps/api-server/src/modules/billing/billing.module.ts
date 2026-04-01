51aec550266ad9cc8d362a4dd385920359206325
adriano.uperttech <adriano.uperttech@gmail.com>
2026-04-01 00:01:52 +0000

    Confiabilidade - ajuste

--- apps/api-server/src/modules/billing/billing.module.ts
+++ apps/api-server/src/modules/billing/billing.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { BillingService } from './billing.service';
-import { BillingController } from './billing.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { BillingController } from './billing.controller';
+import { BillingService } from './billing.service';
 import { Subscription } from './entities/subscription.entity';
-import { LoggerModule } from '../../common/logger/logger.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Subscription]), LoggerModule],
+  imports: [TypeOrmModule.forFeature([Subscription])],
   controllers: [BillingController],
   providers: [BillingService],
   exports: [BillingService],
--- apps/api-server/src/modules/plans/plans.module.ts
+++ apps/api-server/src/modules/plans/plans.module.ts
@@ -3,10 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Plan } from './entities/plan.entity';
 import { PlansService } from './plans.service';
 import { PlansController } from './plans.controller';
-import { LoggerModule } from '../../common/logger/logger.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Plan]), LoggerModule],
+  imports: [TypeOrmModule.forFeature([Plan])],
   providers: [PlansService],
   controllers: [PlansController],
   exports: [PlansService],
