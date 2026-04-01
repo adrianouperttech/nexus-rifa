09e88ea866cbd7dde59936a7f7f49c64399e77fa
adriano.uperttech <adriano.uperttech@gmail.com>
2026-04-01 00:05:02 +0000

    Confiabilidade - ajuste

--- apps/api-server/src/common/logger/logger.module.ts
+++ apps/api-server/src/common/logger/logger.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
 import { LoggerService } from './logger.service';
 
+@Global()
 @Module({
   providers: [LoggerService],
   exports: [LoggerService],
